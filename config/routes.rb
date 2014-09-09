cee5c91c223d2ee15bcf6eda31505f19d363890d
mandym_webdev <mandy@mandym.com>
2014-09-09 11:47:29 -0400

    tagging is working except for the individual show pages

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,10 @@ Rails.application.routes.draw do
     resources :answers
   end
 
-  resources :microposts, only: [:create, :destroy]
+  match 'tagged', to: 'answers#tagged', :as => 'tagged', via: 'get'
 
-  match 'tagged', to: 'microposts#tagged', :as => 'tagged', via: 'get'
-  
+  resources :tags
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
